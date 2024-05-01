e38e5b35df958454df88b5abd507ae308b61c1cc
Gints Kristaps Perkons <gintskristaps13@gmail.com>
2024-05-01 23:02:16 +0300

    made location independent

--- ThaumaEngine/Engine/Shaders/CompileShaders.bat
+++ ThaumaEngine/Engine/Shaders/CompileShaders.bat
@@ -1,4 +1,7 @@
+setlocal enabledelayedexpansion
+
+pushd %~dp0
 if not exist Compiled mkdir Compiled
 %VULKAN_SDK%/bin/glslc.exe Raw/base.vert -o Compiled/base-vert.spv
 %VULKAN_SDK%/bin/glslc.exe Raw/base.frag -o Compiled/base-frag.spv
-
+popd
