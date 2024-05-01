23cb3d14edcda13e4d4d14e1ec264418831a550e
Gints Kristaps Perkons <gintskristaps13@gmail.com>
2024-05-01 21:40:18 +0300

    download binary if not exists

---
+++ Scripts/Setup.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set directory=Vendor\Binaries\Premake\Windows\
+set executable=premake5.exe
+set archiveLink=https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-windows.zip
+
+if "%~1"=="" (
+    echo Provide development environment example: vs2022.
+) else (
+pushd %~dp0
+pushd ..
+
+
+
+if not exist !directory!!executable! (
+    set link=""
+    set destination=""
+    
+    if not exist "Vendor\Binaries\Premake\Windows\Temp" (
+        mkdir "Vendor\Binaries\Premake\Windows\Temp"
+    ) else (
+        echo Directory already exists
+    )
+    
+    powershell.exe -Command "Invoke-WebRequest -Uri '!archiveLink!'  -OutFile !directory!Temp\temp.zip"
+    powershell.exe -Command "Expand-Archive -Path '!directory!Temp\temp.zip' -DestinationPath '!directory!\Temp' -Force"
+    copy "!directory!Temp\!executable!" "!directory!"
+    rd /s /q "!directory!Temp"
+) 
+!directory!!executable! %1
+
+popd
+)   
