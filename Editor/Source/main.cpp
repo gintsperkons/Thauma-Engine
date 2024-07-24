05ffcd5bb1d69f34f44d124750ab95011efcb474
Gints Kristaps Perkons <gintskristaps13@gmail.com>
2024-07-24 16:44:30 +0300

    Initialize Window

--- Editor/Source/main.cpp
+++ Editor/Source/main.cpp
@@ -1,10 +1,10 @@
 #include <Engine.h>
-
+#include <Core/Window/Window.h>
 
 int main(int argc, char* argv[])
 {
 	ThaumaEngine::Engine* engine = ThaumaEngine::Engine::GetInstance();
-	engine->Init();
+	engine->Init(new ThaumaEngine::Window("Thauma Engine",800,600));
 	engine->Run();
 	engine->Terminate();
 	return 0;
--- TestBed/Source/main.cpp
+++ TestBed/Source/main.cpp
@@ -1,11 +1,10 @@
 #include <Engine.h>
+#include <Core/Window/Window.h>
 
-
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	ThaumaEngine::Engine* engine = ThaumaEngine::Engine::GetInstance();
-	engine->Init();
+	engine->Init(new ThaumaEngine::Window("TestBed", 800, 600));
 	engine->Run();
 	engine->Terminate();
 	return 0;
